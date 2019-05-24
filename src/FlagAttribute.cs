951c9f900ff801330c8f99c5563414dfab27a197
Alekaei <alekaei522@gmail.com>
2019-05-24 15:50:32 +0100

    Improved flag attribute

--- src/FlagAttribute.cs
+++ src/FlagAttribute.cs
@@ -5,5 +5,9 @@ namespace Kommands
 	[AttributeUsage(AttributeTargets.Parameter)]
 	public class FlagAttribute : Attribute
 	{
+		public char ShortName { get; }
+		public string LongName { get; }
+
+		public FlagAttribute(char shortName, string longName) { ShortName = shortName; LongName = longName; }
 	}
 }
