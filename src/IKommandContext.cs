65a152982951db529f4334e4ed05e7ba400347af
Alekaei <alekaei522@gmail.com>
2019-05-25 02:30:05 +0100

    Improved command context to include shortcuts to write functions

--- src/IKommandContext.cs
+++ src/IKommandContext.cs
@@ -1,4 +1,6 @@
 ﻿using Kommands.Handlers;
+using System.Drawing;
+using System.Threading.Tasks;
 
 namespace Kommands
 {
@@ -6,5 +8,17 @@ namespace Kommands
 	{
 		ICommandHandler Handler { get; }
 		IExecuter Executer { get; }
+
+		void WriteLine(string text, params object[] args);
+		void WriteLine(Color color, string text, params object[] args);
+
+		Task WriteLineAsync(string text, params object[] args);
+		Task WriteLineAsync(Color color, string text, params object[] args);
+
+		void Write(string text, params object[] args);
+		void Write(Color color, string text, params object[] args);
+
+		Task WriteAsync(string text, params object[] args);
+		Task WriteAsync(Color color, string text, params object[] args);
 	}
 }
