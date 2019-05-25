aa6062ca904a752dba785b839200853777cd6997
Alekaei <alekaei522@gmail.com>
2019-05-25 02:18:40 +0100

    Added exception if flag isnt a bool type

---
+++ src/Exceptions/FlagNotBoolException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Reflection;
+
+namespace Kommands.Exceptions
+{
+	public class FlagNotBoolException : Exception
+	{
+		public MethodInfo MethodInfo { get; }
+		public ParameterInfo ParameterInfo { get; }
+
+		public FlagNotBoolException(MethodInfo methodInfo, ParameterInfo parameterInfo)
+			: base($"Flag parametere isnt of type bool in method `{methodInfo.Name}`, parameter `{parameterInfo.Name}`")
+		{
+			MethodInfo = methodInfo;
+			ParameterInfo = parameterInfo;
+		}
+	}
+}
