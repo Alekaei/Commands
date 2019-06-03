4a65a6016869b14742c1cfe86f1fa590bd96bd42
Alekaei <alekaei522@gmail.com>
2019-06-03 17:30:39 +0100

    Added listcommands command

---
+++ src/DefaultCommands/DefaultCommands.cs
@@ -0,0 +1,18 @@
+﻿using Commands.Classes;
+using Commands.Handlers;
+
+namespace Commands.DefaultCommands
+{
+	public class DefaultCommands
+	{
+		[Command("listcommands")]
+		[Summary("Get a list of all commands")]
+		public static void ListCommand(ICommandContext context)
+		{
+			foreach (Command command in (context.Handler as CommandHandlerBase).Commands.Commands)
+			{
+				context.WriteLine($"{command.Syntax}");
+			}
+		}
+	}
+}
